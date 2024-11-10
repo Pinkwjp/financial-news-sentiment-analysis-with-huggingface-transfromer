91bbcaaabad6b5fb4e408409359f012944f1aaf9
Jianping <pinkwjp@gmail.com>
2024-11-10 13:26:13 -0500

    created main function

--- src/app.py
+++ src/app.py
@@ -1,1 +1,13 @@
-from 
+import pandas as pd
+
+from src.utils import fetch_company_news, create_classifier_pipeline
+
+
+def main(company: str, lang='en', reigon='US'):
+    news_df = fetch_company_news(company=company, lang=lang, reigon=reigon)
+    classifier = create_classifier_pipeline()
+    sentiments = classifier(news_df['title'].to_list())
+    news_and_sentiments_df = pd.concat([news_df, pd.DataFrame(sentiments)], axis=1)
+    return news_and_sentiments_df
+
+
